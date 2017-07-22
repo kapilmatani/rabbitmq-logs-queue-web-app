b598eeb568be0b601dc5ec69282cb018ac4bce9e
kapilmatani <kapilmatani03@gmail.com>
2017-07-22 09:55:38 +0530

    added comments

--- app2/receive_logs.py
+++ app2/receive_logs.py
@@ -1,25 +1,24 @@
-#!/usr/bin/env python
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='logs',
+channel.exchange_declare(exchange='logs',  #defining exchange as logs - logs are pushed from app to exchange and exchange will push to all queue
                          type='fanout')
 
-result = channel.queue_declare(exclusive=True)
+result = channel.queue_declare(exclusive=True)     #declaring a random queue and exclusive=true flag will delete it after we close the connection
 queue_name = result.method.queue
 
 channel.queue_bind(exchange='logs',
-                   queue=queue_name)
+                   queue=queue_name)          #we already created a queue, now binding is use to tell the exchange to send messages to our queue
 
 print('[*] Waiting for logs. To exit press CTRL+C')
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body):  #receiving messages from exchange works by subscribing a callback function [pika library]
     print(body)
 
 channel.basic_consume(callback,
                       queue=queue_name,
-                      no_ack=True)
+                      no_ack=True)         #telling rabbitmq that callback should consume messages from exchange
 
-channel.start_consuming()
+channel.start_consuming()    #never-ending loop that waits for data and runs callback whenever necessary
