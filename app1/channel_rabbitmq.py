e2f8d9a140f14d98ba6090f7e347f8530727b48a
kapilmatani <kapilmatani03@gmail.com>
2017-07-22 09:52:30 +0530

    Update channel_rabbitmq.py

--- app1/channel_rabbitmq.py
+++ app1/channel_rabbitmq.py
@@ -1,7 +1,7 @@
-import pika
+import pika  #imported a python client recommended for using rabbitmq
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-channel = connection.channel()
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))  #connection to localhost
+channel = connection.channel()  #connection via making a channel
 
 channel.exchange_declare(exchange='logs',
-                         type='fanout')
+                         type='fanout')    #declaring queue, fanout exchange will broadcasts all the messages it receives to all known queues.
