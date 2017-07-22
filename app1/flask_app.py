082ffb3abdf7b8dc1a7e587aeed190f25d012eca
kapilmatani <kapilmatani03@gmail.com>
2017-07-22 09:54:32 +0530

    adding comments

--- app1/flask_app.py
+++ app1/flask_app.py
@@ -6,32 +6,31 @@ import time
 from flask import redirect
 from datetime import datetime
 
-def log_rabbitmq(log):
-    channel.basic_publish(exchange='logs',
+def log_rabbitmq(log):                      # python function to publish logs whenever called to a exchange named 'logs'
+    channel.basic_publish(exchange='logs',  # exchange program will push logs to a exchange named logs  
                           routing_key='',
                           body=log)
     return
 
-def log_format(loglevel,message):
+def log_format(loglevel,message):           #python function to return the specified logformat given message input
     return str(datetime.now())[:-3] + ' [' + loglevel + '] ContactPageService - ' + message
 
-app = Flask(__name__)
+app = Flask(__name__)  #initialized flask app
 
-@app.route('/')
+@app.route('/') #default route of flask app i.e. index page
 def index():
-    log_rabbitmq(log_format("INFO", "User landed on Contact Us Page"))
-    return render_template('contact_us.html')
+    log_rabbitmq(log_format("INFO", "User landed on Contact Us Page")) #publishing logs when user landed on contact page.
+    return render_template('contact_us.html')  #returning rendered html page
     
-@app.route('/login', methods=['POST','GET'])
+@app.route('/login', methods=['POST','GET'])   #user will be redirected to /login while submitting contact form.
 def login():
     error = None
-    if request.method == 'POST':
-	log_rabbitmq(log_format("INFO", "User entered Full name: %r" % str(request.form['name'])))	
+    if request.method == 'POST':   #handling post request
+	log_rabbitmq(log_format("INFO", "User entered Full name: %r" % str(request.form['name'])))   #adding whatever user entered in logs
 	log_rabbitmq(log_format("INFO", "User entered Email: %r" % str(request.form['email'])))
         
 	mobile = str(request.form['mobile'])
- 	if len(mobile)!=10 or not mobile.isdigit():
-	    print 'hi'
+ 	if len(mobile)!=10 or not mobile.isdigit():        #checking mobile no format 
 	    log_rabbitmq(log_format("ERROR", "User entered Invalid Mobile no: %r" % mobile))
 	else:
 	    log_rabbitmq(log_format("INFO", "User entered Mobile no: %r" % str(request.form['mobile'])))	
@@ -39,4 +38,4 @@ def login():
 	log_rabbitmq(log_format("INFO", "User entered Reviews: %r" % str(request.form['reviews'])))
 	log_rabbitmq(log_format("INFO", "User Successfully submitted the contact form."))
 
-	return render_template('submit.html')
+	return render_template('submit.html')  #returning another template
